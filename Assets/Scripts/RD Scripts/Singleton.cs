1c9ab61f30fca83d52e8abbf36a2e1cfb587f640
Jordan <jordancog94@gmail.com>
2020-07-26 16:31:33 -0400

    Added some further folders and the Singleton script

---
+++ Assets/Scripts/RD Scripts/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        InitSingleton(this as T);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    private void InitSingleton(T newInstance)
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = newInstance;
+            OnSetSingleton();
+        }
+    }
+
+    protected virtual void OnSetSingleton()
+    {
+
+    }
+}
