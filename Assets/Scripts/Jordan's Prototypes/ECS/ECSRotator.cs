fab2560aa566a61b8fcb2459de54ffe1b4b120db
Jordan <jordancog94@gmail.com>
2020-07-26 17:23:09 -0400

    ECS WIP

---
+++ Assets/Scripts/Jordan's Prototypes/ECS/ECSRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ECSRotator : MonoBehaviour
+{
+    [SerializeField] public float speed;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    private struct Components
+    {
+        public ECSRotator rotator;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+/*        Entities.ForEach((ref ECSRotator rotator, ref RotationQuaternion rotation) =>
+       {
+
+       });*/
+    }
+}
